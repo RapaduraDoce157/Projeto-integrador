374b168176c12839fef1c45b61cd485c03263607
RapaduraDoce157 <125403663+RapaduraDoce157@users.noreply.github.com>
2023-03-13 17:10:49 -0300

    Add files via upload
    V1

---
+++ WinFormsApp2/WinFormsApp2/frmLogin.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WinFormsApp2
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btoEntrar = new System.Windows.Forms.Button();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btoLimpar = new System.Windows.Forms.Button();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboDepartamento = new System.Windows.Forms.ComboBox();
+            this.cboIDdepartamento = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(220, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Senha";
+            // 
+            // btoEntrar
+            // 
+            this.btoEntrar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btoEntrar.Image = ((System.Drawing.Image)(resources.GetObject("btoEntrar.Image")));
+            this.btoEntrar.Location = new System.Drawing.Point(304, 196);
+            this.btoEntrar.Name = "btoEntrar";
+            this.btoEntrar.Size = new System.Drawing.Size(75, 73);
+            this.btoEntrar.TabIndex = 1;
+            this.btoEntrar.UseVisualStyleBackColor = true;
+            this.btoEntrar.Click += new System.EventHandler(this.btoEntrar_Click);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSenha.Location = new System.Drawing.Point(218, 98);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(259, 29);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(220, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Login";
+            // 
+            // btoLimpar
+            // 
+            this.btoLimpar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btoLimpar.Image = ((System.Drawing.Image)(resources.GetObject("btoLimpar.Image")));
+            this.btoLimpar.Location = new System.Drawing.Point(399, 196);
+            this.btoLimpar.Name = "btoLimpar";
+            this.btoLimpar.Size = new System.Drawing.Size(75, 73);
+            this.btoLimpar.TabIndex = 1;
+            this.btoLimpar.UseVisualStyleBackColor = true;
+            this.btoLimpar.Click += new System.EventHandler(this.btoLimpar_Click);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLogin.Location = new System.Drawing.Point(220, 40);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(259, 29);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboDepartamento);
+            this.groupBox1.Controls.Add(this.cboIDdepartamento);
+            this.groupBox1.Controls.Add(this.txtLogin);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtSenha);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btoLimpar);
+            this.groupBox1.Controls.Add(this.btoEntrar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(482, 275);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // cboDepartamento
+            // 
+            this.cboDepartamento.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cboDepartamento.FormattingEnabled = true;
+            this.cboDepartamento.Location = new System.Drawing.Point(356, 152);
+            this.cboDepartamento.Name = "cboDepartamento";
+            this.cboDepartamento.Size = new System.Drawing.Size(121, 29);
+            this.cboDepartamento.TabIndex = 3;
+            // 
+            // cboIDdepartamento
+            // 
+            this.cboIDdepartamento.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cboIDdepartamento.FormattingEnabled = true;
+            this.cboIDdepartamento.Location = new System.Drawing.Point(218, 152);
+            this.cboIDdepartamento.Name = "cboIDdepartamento";
+            this.cboIDdepartamento.Size = new System.Drawing.Size(121, 29);
+            this.cboIDdepartamento.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(356, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Departamento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(218, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ID";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 299);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btoEntrar;
+        private TextBox txtSenha;
+        private Label label2;
+        private Button btoLimpar;
+        private TextBox txtLogin;
+        private GroupBox groupBox1;
+        private ComboBox cboDepartamento;
+        private ComboBox cboIDdepartamento;
+        private Label label4;
+        private Label label3;
+    }
+}
---
+++ WinFormsApp2/WinFormsApp2/frmLogin.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+       
+
+        }
+        public static string stringConexao = "" +
+           "Data Source=localhost;" +
+           "Initial Catalog=ProjetoIntegradorT_13;" +
+           "User ID=sa;" +
+           "Password=123456";
+
+        private void testarconexao()
+        {
+            SqlConnection conn = new SqlConnection(stringConexao);
+
+            try
+            {
+                conn.Open();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+                Application.Exit();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            testarconexao();
+            ComboBox();
+        }
+        public static string IDUsuario = "";
+        public static string NomeUsuario = "";
+        public static string LoginUsuario = "";
+
+        private void ComboBox()
+        {
+            string sql = "select id_departamento,nome_departamento from Departamento";
+            SqlConnection con = new SqlConnection(stringConexao);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+
+            DataTable tabela = new DataTable();
+            con.Open();
+
+            try
+            {
+                reader = cmd.ExecuteReader();
+                tabela.Load(reader);
+
+                cboDepartamento.DisplayMember = "nome_departamento";
+                cboDepartamento.DataSource = tabela;
+
+
+                cboIDdepartamento.DisplayMember = "id_departamento";
+                cboIDdepartamento.DataSource = tabela;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro - " + ex.ToString());
+                Application.Exit();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+      
+    }
+
+        private void btoEntrar_Click(object sender, EventArgs e)
+        {
+            Boolean Valida = false;
+            string Sql = "select * from Usuario where " +
+            "login_Usuario = '" + txtLogin.Text + "' and " +
+            "id_departamento_Usuario = '" + cboIDdepartamento.Text + "' and " +
+            "senha_Usuario = '" + txtSenha.Text + "'"; SqlConnection conexao = new SqlConnection(stringConexao);
+            SqlCommand cmd = new SqlCommand(Sql, conexao);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            conexao.Open();
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    Valida = true;
+
+                    IDUsuario = reader[0].ToString();
+                    NomeUsuario = reader[1].ToString();
+                    LoginUsuario = reader[2].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário ou senha inválidos");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conexao.Close();
+            }
+            if (Valida)
+            {
+                MessageBox.Show("Login feito");
+                //MDIParent1 frm = new MDIParent1();
+                //frm.Show();
+                //this.Hide();
+            }
+        }
+
+        private void btoLimpar_Click(object sender, EventArgs e)
+        {
+            txtLogin.Text = "";
+            txtSenha.Text = "";
+            cboDepartamento.Text = "";
+            cboIDdepartamento.Text = "";
+        }
+    }
+    }
